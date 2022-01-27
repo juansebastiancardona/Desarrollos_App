1d6e14354b82b85ea18958d742dd7bfd1667476c
Juan Sebastian Cardona Lopez <sebithas0625@gmail.com>
2022-01-27 13:56:42 -0500

    figuras geometricas javascript

---
+++ javascript/figuras_geometricas.js
@@ -0,0 +1,11 @@
+const lado = 5;
+
+console.log("el lado del cuadrado es " + lado);
+
+const perimetro = lado + lado + lado +lado;
+
+
+console.log("el perimetro del cuadrado es " + perimetro);
+ const area= lado * lado;
+
+ console.log("el area del cuadrado es " + area);
