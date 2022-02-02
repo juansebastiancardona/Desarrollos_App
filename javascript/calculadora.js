0b4c792a2a7c0c56c0639a6c699ba388e9486cda
Juan Sebastian Cardona Lopez <sebithas0625@gmail.com>
2022-02-02 18:12:59 -0500

    commit de prueba de sincronizacion

--- javascript/calculadora.js
+++ javascript/calculadora.js
@@ -1,11 +1,3 @@
-// triangulo
-
-function perimetrotriangulo(lado1, lado2, base){
-    return (lado1 * lado2);
-}
-
-
-// cuadrado
 function perimetrocuadrado(lado){
     return lado* 4;
 }
@@ -13,24 +5,9 @@ function areacuadrado (lado){
 
     return lado * lado;
 }
+//codigo de prueba
 
 //interactuando con html
-function calcularperitriangulo(){
-    const ladotri1 = document.getElementById("inputrilado1");
-    const ladotri2 = document.getElementById("inputrilado2");
-    const basetri = document.getElementById("inputribase");
-    const alturatri= document.getElementById("inputrialtura");
-
-    const lad1 = ladotri1.value;
-    const lad2 = ladotri2.value;
-    const bas  = basetri.value;
-    const alt  = alturatri.value;
-
-    const peritriangulo = perimetrotriangulo(lad1 , lad2 , bas);
-    alert(peritriangulo);
-
-
-}
 
 function calcularperimetrocuadrado(){
     const ladocuadrado = document.getElementById("inputcuadrado");
@@ -45,7 +22,5 @@ function calcularareacuadrado(){
     const ladocuadrado = document.getElementById("inputcuadrado");
     const value = ladocuadrado.value;
     const area = areacuadrado(value);
-
     alert(area);
 }
-
