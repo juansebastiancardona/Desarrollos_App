30e2ba13ca09cae0120bb879dc76aeba5efd2bed
Juan Sebastian Cardona Lopez <sebithas0625@gmail.com>
2022-02-15 07:35:53 -0500

    objetos en javascript

---
+++ javascript/Objetos/objetos.js
@@ -0,0 +1,59 @@
+const cliente = {
+
+    cedula : "1054992167",
+    nombre : "sebas",
+    edad :20,
+    
+
+    TipoMembresia : ["basica", "premium",  "vip"],
+
+    // metodo
+
+    aprobado(nuevo){
+        this.TipoMembresia.push(nuevo);
+        
+
+    }
+
+
+};
+
+//insertar 
+
+cliente.TipoMembresia.push("oro");
+
+// prototipo
+function student (cedula , nombre ,edad){
+    
+    this.cedula = cedula;
+    this.nombre = nombre;
+    this.edad = edad;
+
+
+    this.TipoMembresia = TipoMembresia;
+
+// primera forma de insertar datos en el array dentro del prototipo
+    // this.aprobado = function (nuevo){
+    //     this.aprobado.push("oro")
+    // }
+
+}
+//segunda forma de hacerlo con este metodo y la herramineta prototypoe
+student.prototype.aprobado = function (nuevo){
+
+    this.aprobado.push("oro");
+}
+
+// creamos instancia de aStudent
+
+const juan = new Student(
+
+    "105455",
+    "JUAN",
+    20,
+    ["DATOS", "ALGORITMOS"]
+
+)
+
+
+
