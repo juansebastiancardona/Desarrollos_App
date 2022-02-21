7a8689af29221cefe351ca773ea444389158a3b4
Juan sebastian cardona <sebitas0625@gmail.com>
2022-02-21 16:34:25 -0500

    curso intermedio poo javascript

---
+++ javascript/intermedio_POO/metodos_static.js
@@ -0,0 +1,10 @@
+const objeto1  = {
+
+    nombre: "sebas",
+    edad: 30,
+    email: "juans@gmail.com"
+}
+
+// esto me muestra una lista de los items
+
+Object.keys(objeto1);
---
+++ javascript/intermedio_POO/static_c1.js
@@ -0,0 +1,20 @@
+class alarma {
+
+    
+
+    static sonidon = "cuak";
+}
+
+console.log(alarma.sonidon);
+
+// funcion
+
+class alarma2 {
+
+    // metodo
+    static sonido2 (){
+        return "cuak";
+    }
+}
+
+console.log(alarma2.sonido2 ());
